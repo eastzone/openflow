35654f61e0abc19cc28552cfe8be6dd13a8f13e4
James Hongyi Zeng <eastzonexp@gmail.com>
2012-07-12 09:13:19 -0700

    openflow: use xparameter.h

--- hw-lib/nf2/reg_defines_openflow_switch.h
+++ hw-lib/nf2/reg_defines_openflow_switch.h
@@ -6,6 +6,7 @@
  *
  * NOTE: This header file has been MANUALLY created
  ********************************************************/
+#include "xparameters.h"
 
 #ifndef _REG_DEFINES_OPENFLOW_SWITCH_
 #define _REG_DEFINES_OPENFLOW_SWITCH_
@@ -146,52 +147,52 @@
 /* ========= Modules ========= */
 
 // Module tags
-#define OF_BASE_ADDR                        0x7a018000
+#define OF_BASE_ADDR                        XPAR_OPENFLOW_DATAPATH_0_S_AXI_BASEADDR
 
 /* ========== Registers ========== */
 
 // Name: openflow_datapath(OF)
 // Description: openflow_datapath
-#define OF_NUM_PKTS_DROPPED_0_REG                0x7a018000
-#define OF_NUM_PKTS_DROPPED_1_REG                0x7a018001
-#define OF_NUM_PKTS_DROPPED_2_REG                0x7a018002
-#define OF_NUM_PKTS_DROPPED_3_REG                0x7a018003
-#define OF_NUM_PKTS_DROPPED_4_REG                0x7a018004
-#define OF_EXACT_HITS_REG                        0x7a018005
-#define OF_EXACT_MISSES_REG                      0x7a018006
-#define OF_WILDCARD_HITS_REG                     0x7a018007
-#define OF_WILDCARD_MISSES_REG                   0x7a018008
-#define OF_DL_PARSE_CNT_0_REG                    0x7a018009
-#define OF_DL_PARSE_CNT_1_REG                    0x7a01800a
-#define OF_DL_PARSE_CNT_2_REG                    0x7a01800b
-#define OF_DL_PARSE_CNT_3_REG                    0x7a01800c
-#define OF_DL_PARSE_CNT_4_REG                    0x7a01800d
-#define OF_MPLS_PARSE_CNT_0_REG                  0x7a01800e
-#define OF_MPLS_PARSE_CNT_1_REG                  0x7a01800f
-#define OF_MPLS_PARSE_CNT_2_REG                  0x7a018010
-#define OF_MPLS_PARSE_CNT_3_REG                  0x7a018011
-#define OF_MPLS_PARSE_CNT_4_REG                  0x7a018012
-#define OF_ARP_PARSE_CNT_0_REG                   0x7a018013
-#define OF_ARP_PARSE_CNT_1_REG                   0x7a018014
-#define OF_ARP_PARSE_CNT_2_REG                   0x7a018015
-#define OF_ARP_PARSE_CNT_3_REG                   0x7a018016
-#define OF_ARP_PARSE_CNT_4_REG                   0x7a018017
-#define OF_IP_TP_PARSE_CNT_0_REG                 0x7a018018
-#define OF_IP_TP_PARSE_CNT_1_REG                 0x7a018019
-#define OF_IP_TP_PARSE_CNT_2_REG                 0x7a01801a
-#define OF_IP_TP_PARSE_CNT_3_REG                 0x7a01801b
-#define OF_IP_TP_PARSE_CNT_4_REG                 0x7a01801c
-#define OF_LAST_OUTSIDE_REG                      0x7a01801c
-#define OF_ACC_RDY_REG                           0x7a01801d
-#define OF_BASE_ADDR_REG                         0x7a01801e
-#define OF_WRITE_ORDER_REG                       0x7a01801f
-#define OF_MOD_WRITE_ORDER_REG                   0x7a018020
-#define OF_READ_ORDER_REG                        0x7a018021
-#define OF_ENTRY_BASE_REG                        0x7a018022
-#define OF_LOOKUP_CMP_BASE_REG                   0x7a018022
-#define OF_LOOKUP_CMP_MASK_BASE_REG              0x7a01802a
-#define OF_LOOKUP_ACTION_BASE_REG                0x7a018032
-#define OF_STATS_BASE_REG                        0x7a01803c
+#define OF_NUM_PKTS_DROPPED_0_REG                OF_BASE_ADDR+0x00
+#define OF_NUM_PKTS_DROPPED_1_REG                OF_BASE_ADDR+0x01
+#define OF_NUM_PKTS_DROPPED_2_REG                OF_BASE_ADDR+0x02
+#define OF_NUM_PKTS_DROPPED_3_REG                OF_BASE_ADDR+0x03
+#define OF_NUM_PKTS_DROPPED_4_REG                OF_BASE_ADDR+0x04
+#define OF_EXACT_HITS_REG                        OF_BASE_ADDR+0x05
+#define OF_EXACT_MISSES_REG                      OF_BASE_ADDR+0x06
+#define OF_WILDCARD_HITS_REG                     OF_BASE_ADDR+0x07
+#define OF_WILDCARD_MISSES_REG                   OF_BASE_ADDR+0x08
+#define OF_DL_PARSE_CNT_0_REG                    OF_BASE_ADDR+0x09
+#define OF_DL_PARSE_CNT_1_REG                    OF_BASE_ADDR+0x0a
+#define OF_DL_PARSE_CNT_2_REG                    OF_BASE_ADDR+0x0b
+#define OF_DL_PARSE_CNT_3_REG                    OF_BASE_ADDR+0x0c
+#define OF_DL_PARSE_CNT_4_REG                    OF_BASE_ADDR+0x0d
+#define OF_MPLS_PARSE_CNT_0_REG                  OF_BASE_ADDR+0x0e
+#define OF_MPLS_PARSE_CNT_1_REG                  OF_BASE_ADDR+0x0f
+#define OF_MPLS_PARSE_CNT_2_REG                  OF_BASE_ADDR+0x10
+#define OF_MPLS_PARSE_CNT_3_REG                  OF_BASE_ADDR+0x11
+#define OF_MPLS_PARSE_CNT_4_REG                  OF_BASE_ADDR+0x12
+#define OF_ARP_PARSE_CNT_0_REG                   OF_BASE_ADDR+0x13
+#define OF_ARP_PARSE_CNT_1_REG                   OF_BASE_ADDR+0x14
+#define OF_ARP_PARSE_CNT_2_REG                   OF_BASE_ADDR+0x15
+#define OF_ARP_PARSE_CNT_3_REG                   OF_BASE_ADDR+0x16
+#define OF_ARP_PARSE_CNT_4_REG                   OF_BASE_ADDR+0x17
+#define OF_IP_TP_PARSE_CNT_0_REG                 OF_BASE_ADDR+0x18
+#define OF_IP_TP_PARSE_CNT_1_REG                 OF_BASE_ADDR+0x19
+#define OF_IP_TP_PARSE_CNT_2_REG                 OF_BASE_ADDR+0x1a
+#define OF_IP_TP_PARSE_CNT_3_REG                 OF_BASE_ADDR+0x1b
+#define OF_IP_TP_PARSE_CNT_4_REG                 OF_BASE_ADDR+0x1c
+#define OF_LAST_OUTSIDE_REG                      OF_BASE_ADDR+0x1c
+#define OF_ACC_RDY_REG                           OF_BASE_ADDR+0x1d
+#define OF_BASE_ADDR_REG                         OF_BASE_ADDR+0x1e
+#define OF_WRITE_ORDER_REG                       OF_BASE_ADDR+0x1f
+#define OF_MOD_WRITE_ORDER_REG                   OF_BASE_ADDR+0x20
+#define OF_READ_ORDER_REG                        OF_BASE_ADDR+0x21
+#define OF_ENTRY_BASE_REG                        OF_BASE_ADDR+0x22
+#define OF_LOOKUP_CMP_BASE_REG                   OF_BASE_ADDR+0x22
+#define OF_LOOKUP_CMP_MASK_BASE_REG              OF_BASE_ADDR+0x2a
+#define OF_LOOKUP_ACTION_BASE_REG                OF_BASE_ADDR+0x32
+#define OF_STATS_BASE_REG                        OF_BASE_ADDR+0x3c
 
 #endif
 
